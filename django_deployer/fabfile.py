ba59cfa60d7a178ff9153cf05f2b8604f1864eb7
John Debs <johnthedebs@gmail.com>
2012-09-07 14:54:14 -0400

    Add provider config template output and a little organization

--- django_deployer/fabfile.py
+++ django_deployer/fabfile.py
@@ -1,11 +1,22 @@
+# -*- coding: utf-8 -*-
 import yaml
 import os
-import posixpath
 import pkg_resources
+
+from jinja2 import Environment, PackageLoader
 
 from fabric.api import *
 from fabric.colors import green, red, yellow
-from fabric.contrib.files import exists, contains, upload_template
+
+
+DEPLOY_YAML = os.path.join(os.getcwd(), 'deploy.yml')
+
+template_env = Environment(loader=PackageLoader('django_deployer', 'paas_templates'))
+
+
+#
+# Tasks
+#
 
 def init():
     _green("We need to ask a few questions before we can deploy your Django app")
@@ -18,18 +29,65 @@ def init():
             'django_settings': django_settings
             }
 
+def deploy(provider=None):
+    site = init()
+
+    if not provider:
+        provider = prompt("Which provider would you like to deploy to?", default="stackato")
+
+    _create_deploy_yaml(site, provider)
+    _create_provider_configs()
+
+
+#
+# Helpers
+#
+
 def _create_deploy_yaml(site, provider):
     site_yaml_dict = site
     site_yaml_dict['provider'] = provider
-    file = _join(os.getcwd(), 'deploy.yml')
-    _write_file(file, yaml.safe_dump(site_yaml_dict, default_flow_style=False))
+    _write_file(DEPLOY_YAML, yaml.safe_dump(site_yaml_dict, default_flow_style=False))
+
+def _create_provider_configs():
+    site = yaml.safe_load(_read_file(DEPLOY_YAML))
+
+    provider = site['provider']
+
+    yaml_template_name     = os.path.join(provider, '%s.yml' % provider)
+    settings_template_name = os.path.join(provider, 'settings_%s.py' % provider)
+
+    yaml_template     = template_env.get_template(yaml_template_name)
+    settings_template = template_env.get_template(settings_template_name)
+
+    yaml_contents     = yaml_template.render(**site)
+    settings_contents = settings_template.render(**site)
+
+    settings_path = site['django_settings'].replace('.', '/') + '_%s.py' % provider
+
+    _write_file('%s.yml' % provider, yaml_contents)
+    _write_file(settings_path, settings_contents)
 
 def _write_file(path, contents):
     file = open(path, 'w')
     file.write(contents)
     file.close()
 
-# pretty colors
+def _read_file(path):
+    file = open(path, 'r')
+    contents = file.read()
+    file.close()
+    return contents
+
+def _join(*args):
+    """Convenience wrapper around os.path.join to make the rest of our
+    functions more readable."""
+    return os.path.join(*args)
+
+
+#
+# Pretty colors
+#
+
 def _green(text):
     print green(text)
 
@@ -38,16 +96,3 @@ def _red(text):
 
 def _yellow(text):
     print yellow(text)
-
-def _join(*args):
-    """Convenience wrapper around posixpath.join to make the rest of our
-    functions more readable."""
-    return posixpath.join(*args)
-
-def deploy(provider=None):
-    site = init()
-
-    if not provider:
-        provider = prompt("Which provider would you like to deploy to?", default="stackato")
-
-    _create_deploy_yaml(site, provider)
