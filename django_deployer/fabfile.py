1b12df762ec39cf002864aa146f2991543356bfc
Nate Aune <natea@jazkarta.com>
2012-09-07 13:28:37 -0400

    very basic fabfile that asks some questions about the django project and generates a yaml file

---
+++ django_deployer/fabfile.py
@@ -0,0 +1,48 @@
+import yaml
+import os
+import posixpath
+import pkg_resources
+
+from fabric.api import *
+from fabric.colors import green, red, yellow
+from fabric.contrib.files import exists, contains, upload_template
+
+def init():
+    _green("We need to ask a few questions before we can deploy your Django app")
+    pyversion = prompt("What version of Python does your app need?", default="Python 2.7")
+    database = prompt("What database does your app use?", default="PostgreSQL")
+    django_settings = prompt("What is your Django settings module?", default="%s.settings" % os.path.basename(os.getcwd()))
+
+    return {'pyversion': pyversion,
+            'database': database,
+            'django_settings': django_settings
+            }
+
+def _create_deploy_yaml(site):
+    site_yaml_dict = site
+    file = _join(os.getcwd(), 'deploy.yml')
+    _write_file(file, yaml.safe_dump(site_yaml_dict, default_flow_style=False))
+
+def _write_file(path, contents):
+    file = open(path, 'w')
+    file.write(contents)
+    file.close()
+
+# pretty colors
+def _green(text):
+    print green(text)
+
+def _red(text):
+    print red(text)
+
+def _yellow(text):
+    print yellow(text)
+
+def _join(*args):
+    """Convenience wrapper around posixpath.join to make the rest of our
+    functions more readable."""
+    return posixpath.join(*args)
+
+def deploy():
+    site = init()
+    _create_deploy_yaml(site)
