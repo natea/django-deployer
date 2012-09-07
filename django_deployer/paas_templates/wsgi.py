48cb5232f6fcd470a14cb62631f07fb8f8f383f0
Nate Aune <natea@jazkarta.com>
2012-09-07 16:18:34 -0400

    same wsgi.py file should work across all PaaS providers

---
+++ django_deployer/paas_templates/wsgi.py
@@ -0,0 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'{{ project_name }}')))
+os.environ['DJANGO_SETTINGS_MODULE'] = '{{ django_settings }}'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
