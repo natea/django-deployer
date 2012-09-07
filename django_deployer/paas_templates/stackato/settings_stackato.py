bb4606601ffa73bfe19b548f9660300a5303e330
Nate Aune <natea@jazkarta.com>
2012-09-07 14:50:34 -0400

    remove gondor references

--- django_deployer/paas_templates/stackato/settings_stackato.py
+++ django_deployer/paas_templates/stackato/settings_stackato.py
@@ -11,8 +11,8 @@ DATABASES = {
 
 SITE_ID = 1 # set this to match your Sites setup
 
-MEDIA_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "media")
-STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "static")
+MEDIA_ROOT = os.path.join(os.environ["UWSGI_STATIC_URL"], "site_media", "media")
+STATIC_ROOT = os.path.join(os.environ["UWSGI_STATIC_URL"], "site_media", "static")
 
 MEDIA_URL = "/site_media/media/" # make sure this maps inside of site_media_url
 STATIC_URL = "/site_media/static/" # make sure this maps inside of site_media_url
