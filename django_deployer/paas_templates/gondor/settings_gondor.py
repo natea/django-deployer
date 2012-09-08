53bb2d9c91471547275e6b1b731867e45a2d1fa7
Nate Aune <natea@jazkarta.com>
2012-09-08 13:23:53 -0400

    add logging back

--- django_deployer/paas_templates/gondor/settings_gondor.py
+++ django_deployer/paas_templates/gondor/settings_gondor.py
@@ -4,9 +4,37 @@ import dj_database_url
 
 from .settings import *
 
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
     "default": dj_database_url.config(env="GONDOR_DATABASE_URL"),
 }
 
 MEDIA_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "media")
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(levelname)s %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple"
+        }
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "django.request": {
+            "propagate": True,
+        },
+    }
+}
--- django_deployer/paas_templates/stackato/settings_stackato.py
+++ django_deployer/paas_templates/stackato/settings_stackato.py
@@ -3,9 +3,37 @@ import dj_database_url
 
 from .settings import *
 
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
-    "default": dj_database_url.config(env="DATABASE_URL"),
+    "default": dj_database_url.config(env["DATABASE_URL"]),
 }
 
 MEDIA_ROOT = os.environ['STACKATO_FILESYSTEM']
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(levelname)s %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple"
+        }
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "django.request": {
+            "propagate": True,
+        },
+    }
+}
