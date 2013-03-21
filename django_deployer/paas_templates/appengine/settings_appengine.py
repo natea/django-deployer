700bfc834eebae728c19662b236d195f5ed3981b
LittleQ <littleq0903@gmail.com>
2013-03-20 23:33:29 -0700

    fixed some bugs in settings_appengine

--- django_deployer/paas_templates/appengine/settings_appengine.py
+++ django_deployer/paas_templates/appengine/settings_appengine.py
@@ -1,5 +1,6 @@
 from .settings import *
 
+# use cloudsql while on the production
 if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
     os.getenv('SETTINGS_MODE') == 'prod'):
     # Running on production App Engine, so use a Google Cloud SQL database.
@@ -10,17 +11,33 @@ if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
             'NAME': '{{ databasename }}',
         }
     }
+elif not DATABASES:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'django_deployer_default',
+            'USER': '',
+            'PASSWORD': '',
+        }
+    }
 
 
+
+# django email backend for appengine
 EMAIL_BACKEND = 'rocket_engine.email.EmailBackend'
 
+INSTALLED_APPS = tuple()
+
+# Installed apps for django-deployer
 PAAS_INSTALLED_APPS = (
-    'rocket_engine'
+    'rocket_engine',
 )
 
+try:
+    INSTALLED_APPS = tuple(list(INSTALLED_APPS) + list(PAAS_INSTALLED_APPS))
+except NameError:
+    pass
+
+# use Blob datastore for default file storage
 DEFAULT_FILE_STORAGE = 'rocket_engine.storage.BlobStorage'
 
-if INSTALLED_APPS:
-    INSTALLED_APPS += PAAS_INSTALLED_APPS
-else:
-    INSTALLED_APPS = PAAS_INSTALLED_APPS
