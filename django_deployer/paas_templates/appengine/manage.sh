38748079a21f00cca3191b382fe63160a85ee98c
Nate Aune <natea@jazkarta.com>
2013-03-26 15:55:21 -0400

    remove /bin/sh as it messes things up, and add a TODO about putting the PYTHONPATH declaraion in front of manage.py

--- django_deployer/paas_templates/appengine/manage.sh
+++ django_deployer/paas_templates/appengine/manage.sh
@@ -1,8 +1,8 @@
-!#/bin/sh
-
 env/bin/python {{ project_name }}/manage.py $@ --settings={{ project_name }}.settings_appengine
 APPENGINE_SDK_LOCATION="{{ sdk_location }}"
 args=$@
+
+# TODO: i think this line needs to go in front of the manage.py command. 
 export PYTHONPATH="env/lib/python2.7:$APPENGINE_SDK_LOCATION:$APPENGINE_SDK_LOCATION/lib/django-1.4"
 export DJANGO_SETTINGS_MODULE="{{ project_name }}.settings_appengine"
 
