fd2b02b35c432e87bd1734b90e6122ca68d4e3fa
Nate Aune <natea@jazkarta.com>
2013-03-26 14:27:17 -0400

    renamed to make it more clear that it's a shell script and modified to use oauth2

--- django_deployer/paas_templates/appengine/manage.sh
+++ django_deployer/paas_templates/appengine/manage.sh
@@ -1,3 +1,5 @@
+!#/bin/sh
+
 env/bin/python {{ project_name }}/manage.py $@ --settings={{ project_name }}.settings_appengine
 APPENGINE_SDK_LOCATION="{{ sdk_location }}"
 args=$@
@@ -5,7 +7,7 @@ export PYTHONPATH="env/lib/python2.7:$APPENGINE_SDK_LOCATION:$APPENGINE_SDK_LOCA
 export DJANGO_SETTINGS_MODULE="{{ project_name }}.settings_appengine"
 
 if [ $1 == "deploy" ] ; then
-    appcfg.py update .
+    appcfg.py --oauth2 update .
 else
     env/bin/python mywebsite/manage.py $@
 fi
