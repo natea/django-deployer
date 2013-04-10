be6ea0d8738857a1aeed5621506a02d85c84b401
Nate Aune <natea@jazkarta.com>
2013-04-10 15:41:48 -0400

    make setting the password configurable. need to warn user not to commit mkadmin.py to source control

--- django_deployer/paas_templates/dotcloud/mkadmin.py
+++ django_deployer/paas_templates/dotcloud/mkadmin.py
@@ -3,7 +3,7 @@ from wsgi import *
 from django.contrib.auth.models import User
 u, created = User.objects.get_or_create(username='admin')
 if created:
-    u.set_password('password')
+    u.set_password('{{ admin_password }}')
     u.is_superuser = True
     u.is_staff = True
     u.save()
