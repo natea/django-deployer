eb024c17d2b1c1957f2e57d4a376cfa51a59be07
Nate Aune <natea@jazkarta.com>
2013-04-10 15:40:39 -0400

    some cleanups

--- django_deployer/helpers.py
+++ django_deployer/helpers.py
@@ -2,12 +2,10 @@ import os
 import re
 import yaml
 
-from fabric.api import prompt
 from fabric.colors import green, red, yellow
 
 
 DEPLOY_YAML = os.path.join(os.getcwd(), 'deploy.yml')
-
 
 
 #
@@ -19,6 +17,7 @@ def _create_deploy_yaml(site):
     _write_file(DEPLOY_YAML, yaml.safe_dump(site, default_flow_style=False))
     _green("Created %s" % DEPLOY_YAML)
 
+
 def _validate_django_settings(django_settings):
     django_settings_regex = r"^[\d\w_.]+$"
 
@@ -29,11 +28,12 @@ def _validate_django_settings(django_settings):
     django_settings_path = django_settings.replace('.', '/') + '.py'
     if not os.path.exists(django_settings_path):
         raise ValueError(red(
-            "Couldn't find a settings file at that dotted path.\n" \
+            "Couldn't find a settings file at that dotted path.\n"
             "Make sure you're using django-deployer from your project root."
         ))
 
     return django_settings
+
 
 def _validate_project_name(project_name):
     project_name_regex = r"^.+$"
@@ -44,47 +44,45 @@ def _validate_project_name(project_name):
 
     if not os.path.exists(os.path.join(os.getcwd(), project_name)):
         raise ValueError(red(
-            "Couldn't find that directory name under the current directory.\n" \
+            "Couldn't find that directory name under the current directory.\n"
             "Make sure you're using django-deployer from your project root."
         ))
 
     return project_name
 
 
-# TODO: Finish this up
-# Needs to check whether there's a requirements.txt in the root
-# If not, we ask them where their requirements file is and we can
-# write a requirements.txt in the project's root that also points
-# to theirs.
 def _validate_requirements(requirements):
-    requirements_regex = r"^.+requirements.txt$"
 
-    pattern = re.compile(requirements_regex)
-    if not pattern.match(requirements):
-        raise ValueError(red("You must enter the relative path to your requirements.txt file to continue!"))
+    if not requirements.endswith(".txt"):
+        raise ValueError(red("Requirements file must end with .txt"))
 
     if not os.path.exists(os.path.join(os.getcwd(), requirements)):
         raise ValueError(red(
-            "Couldn't find requirements.txt at the path you gave.\n" \
+            "Couldn't find requirements.txt at the path you gave.\n"
             "Make sure you're using django-deployer from your project root."
         ))
 
     return requirements
+
 
 def _validate_managepy(managepy):
     managepy_regex = r"^.+manage.py$"
 
     pattern = re.compile(managepy_regex)
     if not pattern.match(managepy):
-        raise ValueError(red("You must enter the relative path to your manage.py file to continue!"))
+        raise ValueError(red(
+            "Couldn't find manage.py at the path you gave.\n"
+            "You must enter the relative path to your manage.py file to continue!"
+        ))
 
     if not os.path.exists(os.path.join(os.getcwd(), managepy)):
         raise ValueError(red(
-            "Couldn't find manage.py at the path you gave.\n" \
+            "Couldn't find manage.py at the path you gave.\n"
             "Make sure you're using django-deployer from your project root."
         ))
 
     return managepy
+
 
 #
 # Utils
@@ -95,11 +93,13 @@ def _write_file(path, contents):
     file.write(contents)
     file.close()
 
+
 def _read_file(path):
     file = open(path, 'r')
     contents = file.read()
     file.close()
     return contents
+
 
 def _join(*args):
     """
@@ -116,8 +116,10 @@ def _join(*args):
 def _green(text):
     print green(text)
 
+
 def _red(text):
     print red(text)
+
 
 def _yellow(text):
     print yellow(text)
